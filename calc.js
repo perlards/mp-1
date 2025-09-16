3ddd852aba996ff0c9c9c2bf0c327bd0cb504df4
perla ruiz <perlards@bu.edu>
2025-09-16 01:46:27 -0400

    finished content of resume website

---
+++ calc.js
@@ -0,0 +1,5 @@
+<label for="one">Give me a number:</label>
+<input id="one">
+    <label for="two">Give me a number:</label>
+    <input id="two">
+        <button onclick="onAdd()">Add</button>
---
+++ inex.js
@@ -0,0 +1,8 @@
+let one= document.getElementById('one');
+let two= document.getElementById('two');
+let output=document.getElementById('output');
+
+function doAdd(){
+    output.innerHTML=String(Number(one.value) + Number(two.value));
+
+}
