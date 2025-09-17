d0a36226a9d518fc77c2e66895166615729b938d
perla ruiz <perlards@bu.edu>
2025-09-16 21:23:50 -0400

    implemented power function and negative cases

--- calc.js
+++ calc.js
@@ -4,19 +4,49 @@ let output=document.getElementById('output');
 
 function doAdd(){
     output.innerHTML=String(Number(one.value) + Number(two.value));
-
+    if(Number(output.innerHTML) < 0){
+        output.style.color = "red";
+    } else {
+        output.style.color = "";
+    }
 }
 function doMult(){
     output.innerHTML=String(Number(one.value) * Number(two.value));
-
+    if(Number(output.innerHTML) < 0){
+        output.style.color = "red";
+    } else {
+        output.style.color = "";
+    }
 }
 function doSubtract(){
     output.innerHTML=String(Number(one.value) - Number(two.value));
-
+    if(Number(output.innerHTML) < 0){
+        output.style.color = "red";
+    } else {
+        output.style.color = "";
+    }
 }
 function doDivide(){
     output.innerHTML=String(Number(one.value) / Number(two.value));
-
+    if(Number(output.innerHTML) < 0){
+        output.style.color = "red";
+    } else {
+        output.style.color = "";
+    }
+}
+function doPower() {
+    let base = Number(one.value);
+    let exponent = Number(two.value);
+    let result = 1;
+    for (let i = 0; i < exponent; i++) {
+        result *= base;
+    }
+    output.innerHTML = String(result);
+    if(Number(output.innerHTML) < 0){
+        output.style.color = "red";
+    } else {
+        output.style.color = "";
+    }
 }
 function doClear(){
     output.innerHTML= one.value =""; two.value ="";
