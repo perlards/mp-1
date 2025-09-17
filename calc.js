f258929111ca7ecefeca7aa83fa5c58fa32faad6
perla ruiz <perlards@bu.edu>
2025-09-16 20:57:39 -0400

    resume website deployment ready

--- calc.js
+++ calc.js
@@ -1,5 +1,23 @@
-<label for="one">Give me a number:</label>
-<input id="one">
-    <label for="two">Give me a number:</label>
-    <input id="two">
-        <button onclick="onAdd()">Add</button>
+let one= document.getElementById('one');
+let two= document.getElementById('two');
+let output=document.getElementById('output');
+
+function doAdd(){
+    output.innerHTML=String(Number(one.value) + Number(two.value));
+
+}
+function doMult(){
+    output.innerHTML=String(Number(one.value) * Number(two.value));
+
+}
+function doSubtract(){
+    output.innerHTML=String(Number(one.value) - Number(two.value));
+
+}
+function doDivide(){
+    output.innerHTML=String(Number(one.value) / Number(two.value));
+
+}
+function doClear(){
+    output.innerHTML= one.value =""; two.value ="";
+}
